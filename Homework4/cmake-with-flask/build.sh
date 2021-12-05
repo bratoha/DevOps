c95e43b60753292a4a42c08f84ffbdb9d07354fd
Anton Kalinin <bratohalo@gmail.com>
2021-12-05 15:30:04 +0300

    Homework 4

---
+++ Homework4/cmake-with-flask/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build -t cmake-with-flask:alpine3.15 .
---
+++ Homework4/cmake-with-flask/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run --rm --name cmake-with-flask-alpine3.15 -p 5000:5000 cmake-with-flask:alpine3.15
